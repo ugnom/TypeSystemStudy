66c57573567a6ebf2862d95990edb0ff201706a9
Yuji Nomura <ugnom1121@gmail.com>
2015-07-15 17:26:21 -0400

    commit
    commit

--- UntypedLambda.hs
+++ UntypedLambda.hs
@@ -1,4 +1,4 @@
-
+import Control.Monad.State
 
 
 data LamTerm a =  Var a
@@ -10,6 +10,40 @@ data LamTerm a =  Var a
 -- |x.(z x) (|y.(w v) k)
 -- x y
 parse :: String -> Maybe (LamTerm String)
+parse xs = do
+	dividedStr <- divideToBlocks xs
+	if length dividedStr == 1 then do
+		case (head . head) dividedStr of
+			'|' -> parseLambda . head $ dividedStr
+			_ -> return $ Var (head dividedStr) 
+	else do
+	parsedDividedStr <-  mapM parse dividedStr
+	return . applyApp $ parsedDividedStr
+
+parseLambda :: String -> Maybe (LamTerm String)
+parseLambda xs = undefined
+
+applyApp :: [LamTerm String] -> LamTerm String
+applyApp (x:[]) = x
+applyApp (x:y:xs) = applyApp ((App x y) : xs)
+
+{-
+parse (' ' : xs) = parse xs
+parse all@('(' : xs) = do
+	(f, rest) <- getFirstBlock all
+	f' <- parse f
+	if rest == [] then do
+		return f'
+	else do
+		(s, rest') <- getFirstBlock rest
+		s' <- parse s
+		if rest' == [] then do 
+			return $ App f' s'
+		else do
+			rest'' <- parse rest'
+			return $ App (App f' s') rest''
+
+
 parse ('|' : xs) = do
 	(x, abt, apt, rest) <- findAbs xs
 	if apt == [] then do 
@@ -24,7 +58,7 @@ parse ('|' : xs) = do
 			lamRest <- parse rest
 			return (App (App (Lam x lamAbt) lamApt) lamRest) 
 parse all@(a : xs) = do
-	(v, rest) <- findVar all
+	(v, rest) <- getFirstBlock all
 	if rest == [] then do
 		return (Var v)
 	else do
@@ -32,6 +66,9 @@ parse all@(a : xs) = do
 
 		return (App (Var v) lamRest)
 parse [] = Nothing
+-}
+
+
 
 findAbs :: String -> Maybe (String, String, String, String)
 findAbs xs = do 
@@ -41,26 +78,17 @@ findAbs xs = do
 	(abt, rest) <- getFirstBlock $ (drop (n+1) xs)
 	--abt <- return $ take nn ((drop (n+1)) xs)
 	--rest <- return $ drop (nn+ 2 + (length x)) xs
-	(apt, rest') <- findApAbs rest
+	(apt, rest') <- getFirstBlock rest
 	return (x, abt, apt, rest')
 
-findVar :: String -> Maybe (String, String)
-findVar = getFirstBlock
---findVar xs = do
-	--n <- return . (findIndexOf ' ') $  xs
-	--x <- return $ take n xs
-	--rest <- return $ drop (n+1) xs
-	--return (x , rest)
+divideToBlocks :: String -> Maybe [String]
+divideToBlocks [] = Just []
+divideToBlocks xs = do
+	(f, rest) <- getFirstBlock xs
+	ys <- divideToBlocks rest
+	return (f : ys)
 
-findApAbs :: String -> Maybe (String, String)
-findApAbs = getFirstBlock
-{-
-findApAbs xs = do
-	n <- return . (findIndexOf ' ') $ xs
-	x <- return $ take n xs
-	rest <- return $ drop ((length x) + 1) xs
-	return (x, rest)
--}
+
 findIndexOf :: Char -> String -> Int
 findIndexOf _ [] = 0
 findIndexOf s (x:xs) 
@@ -70,8 +98,18 @@ findIndexOf s (x:xs)
 findBlockEndIndex :: String -> Maybe Int
 findBlockEndIndex [] = Just 0
 findBlockEndIndex all@('(':xs) = findBracketEnd all
+findBlockEndIndex all@('|':xs) = findLambdaEnd all
+findBlockEndIndex (' ':xs) = do 
+	n <- (findBlockEndIndex xs)
+	return (n+1)
 findBlockEndIndex xs = (return . (findIndexOf ' ')) xs
-		
+
+findLambdaEnd :: String -> Maybe Int
+findLambdaEnd ('|':xs) = do
+	n <- return . (findIndexOf '.') $ xs
+	m <- findBlockEndIndex (drop (n+1) xs)
+	return (n + m + 1 + 1)
+
 
 findBracketEnd :: String -> Maybe Int 
 findBracketEnd [] = Just 0
@@ -89,23 +127,28 @@ findBracketEnd ('(':xs) = inFindBracketEnd 1 xs
 		inFindBracketEnd n (x:xs) = do
 			m <- inFindBracketEnd n xs
 			return (m+1)
-
 findBracketEnd xs = Just 0
 
 getFirstBlock :: String -> Maybe (String, String)
 getFirstBlock xs = do
 	n <- findBlockEndIndex xs
-	return ((removeFirstBracket (take (n+1) xs)), (removeFirstBracket (drop (n+1) xs)))
+	return (((trim " ") . (take (n+1))) xs , ((trim " ") . (drop (n+1))) xs)
 
-removeFirstBracket :: String -> String
-removeFirstBracket all@('(':xs) = 
-	if ((head . reverse) xs) == ')' then 
+removeFirstBracket :: String -> Maybe String
+removeFirstBracket all@('(':xs) = do
+	n <- findBracketEnd all
+	if (n + 1) == (length all) then 
 		(removeFirstBracket . reverse . tail . reverse . tail) all
 	else 
-		all
-removeFirstBracket xs = xs
+		Just all
+removeFirstBracket xs = Just xs
+
+trimSpace :: String -> String
+trimSpace = trim " "
 
 trim :: [Char] -> String -> String
+trim _ [] = []
+trim [] xs = xs
 trim xs ys = 
 	let
 		res' = removeHeads xs ys
